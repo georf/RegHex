368a1b0793126e7fd2cfa24e0e22ed24050b3de6
georf <georf@georf.de>
2011-12-13 11:20:39 +0100

    * Remove testing spans * Add some more comments * Remove old unneeded functions * Clean up code

--- js/UIMatchText.js
+++ js/UIMatchText.js
@@ -1,8 +1,10 @@
 /**
- *
+ * 
  * @author Georg Limbach <georf@dev.mgvmedia.com>
- * @param jQuery selection - the matchtext block
- * @param boolean create "delete" button
+ * @param jQuery
+ *            selection - the matchtext block
+ * @param boolean
+ *            create "delete" button
  */
 function UIMatchText(block, deleteButton) {
 	var $this = this;
@@ -32,22 +34,21 @@ function UIMatchText(block, deleteButton) {
 		btn.click(function() {
 			block.slideUp(function() {
 				// remove from RegHex
-				RegHex.removeMatchText($(this).find('.textarea'));
-				$(this).remove();
-			});
+					RegHex.removeMatchText($(this).find('.textarea'));
+					$(this).remove();
+				});
 		});
 
 		// add to block
-		this.block.find('.navigate-match-section').prepend('&nbsp;').prepend(btn);
+		this.block.find('.navigate-match-section').prepend('&nbsp;').prepend(
+				btn);
 		this.block.hide();
 		$('#matching-blocks').append(this.block);
 		this.block.show();
 	}
 
-
 	this.cursorPosition = 0;
 	this.charCount = 0;
-
 
 	this.response = null;
 	this.selectedMatch = 0;
@@ -73,207 +74,120 @@ function UIMatchText(block, deleteButton) {
 				this.updateInfobox();
 			}
 		}
-	}
+	};
 
+	/**
+	 * Highlight the matches in the textarea
+	 * 
+	 * @return this
+	 */
 	this.highlight = function() {
-		if (this.response.error) {
-			this.highlightArea.html('');
-		} else {
-			this.highlightArea.html(this.getHighlightedHtml(this.response.matchText));
-		}
-	}
 
+		// on error highlight nothing
+		if (!this.response || this.response.error) {
+			this.highlightArea.html('');
+
+			// otherwise highlight selected part
+		} else {
+
+			var text = this.response.matchText;
+			var output = "";
+
+			// no matches
+			if (this.response.matchings.length == 0) {
+			
+				// all unmatching
+				output = '<span class="unmatched">' + this.escapeHtml(text) + '</span>';
+			
+			} else {
+
+				var length = this.response.matchings[this.selectedMatch].text.length;
+				var pos = this.response.matchings[this.selectedMatch].index;
+
+				if (length <= 0) {
+					output = '<span class="unmatched">' + this.escapeHtml(text) + '</span>';
+				} else {
+
+					if (pos != 0) {
+						output += '<span class="unmatched">' + this
+								.escapeHtml(text.substring(0, pos)) + '</span>';
+					}
+
+					output += '<span class="matched">' + this.escapeHtml(text
+							.substring(pos, pos + length)) + '</span>';
+
+					if (text.length != pos + length) {
+						output += '<span class="unmatched">' + this
+								.escapeHtml(text.substring(pos + length)) + '</span>';
+					}
+				}
+			}
+
+			// set spans to background area
+			this.highlightArea.html(output);
+		}
+
+		return this;
+	};
+
+	/**
+	 * Escape special chars
+	 * 
+	 * @param String
+	 *            text to escape
+	 * @return String escaped text
+	 */
 	this.escapeHtml = function(text) {
 		text = this.replaceAll(text, '<', '&lt;');
 		text = this.replaceAll(text, '>', '&gt;');
 		text = this.replaceAll(text, '\n', '<br>');
 		return text;
-	}
+	};
+
+	/**
+	 * Replaces each substring of the value that matches the given string with
+	 * the given replacement.
+	 * 
+	 * @param String
+	 *            value
+	 * @param String
+	 *            search for
+	 * @param String
+	 *            replace with
+	 * @return String new text
+	 */
 	this.replaceAll = function(value, find, replace) {
-        while (value.indexOf(find) != -1) {
-            value = value.replace(find, replace);
-        }
-        return value;
-    }
-
-	this.getHighlightedHtml = function(text) {
-		if (this.response.matchings.length == 0) {
-			// no matches
-			return '<span class="unmatched">' + this.escapeHtml(text) + '</span>';
+		while (value.indexOf(find) != -1) {
+			value = value.replace(find, replace);
 		}
-
-		var length = this.response.matchings[this.selectedMatch].text.length;
-		var pos = this.response.matchings[this.selectedMatch].index;
-
-		if (length <= 0) {
-			return '<span class="unmatched">' + this.escapeHtml(text) + '</span>';
-		}
-
-		var output = '';
-
-		if (pos != 0) {
-			output += '<span class="unmatched">' + this.escapeHtml(text.substring(0, pos)) + '</span>';
-		}
-
-		output += '<span class="matched">' + this.escapeHtml(text.substring(pos, pos + length)) + '</span>';
-
-		if (text.length != pos + length) {
-			output += '<span class="unmatched">' + this.escapeHtml(text.substring(pos + length)) + '</span>';
-		}
-
-		return output;
-    }
-
-	this.highlight2 = function() {
-//TODO caching
-		var div = document.getElementById (this.area.attr('id'));
-
-		this.cursorPosition = this.getCursorPosition();
-		this.charCount = 0;
-
-console.debug("cursor", this.cursorPosition);
-
-		// clean html
-		this.makeFlat(div);
-
-		// get selected position
-		var offsetStart = null, offsetEnd = null;
-
-		if (this.response.matchings.length > 0) {
-
-			offsetStart = this.response.matchings[this.selectedMatch].index;
-			offsetEnd = offsetStart + this.response.matchings[this.selectedMatch].text.length;
-
-			for (var i = offsetStart; i < offsetEnd; i++) {
-				this.charCount = i;
-				this.createHighlightSpan(div.childNodes[i]);
-			}
-		}
-	}
-
-	this.createHighlightSpan = function (textNode) {
-		var span = document.createElement('span');
-		span.className = 'matched';
-		span.appendChild(document.createTextNode(textNode.data));
-		this.insert(textNode.parentNode, span, textNode);
-
-		textNode.parentNode.removeChild(textNode);
-	}
-
-
-	this.makeFlat = function (element) {
-
-		var children = new Array();
-		for (var i = 0; i < element.childNodes.length; i++) {
-			children[i] = element.childNodes[i];
-		}
-
-		for (var i = 0; i < children.length; i++) {
-			while (children[i].hasChildNodes()) {
-				var node = this.getFirstTextNode(children[i].childNodes[0]);
-				if (node != null) {
-					this.insertSeparately(element, node, children[i]);
-				}
-			}
-			if (children[i].nodeType != 3) {
-				console.debug("remove empty element", children[i]);
-				element.removeChild(children[i]);
-			} else if (children[i].data.length != 1) {
-				this.insertSeparately(element, children[i], children[i]);
-				console.debug("remove long text element", children[i]);
-				element.removeChild(children[i]);
-			}
-
-		}
-	}
-
-	this.getFirstTextNode = function (element) {
-
-		if (element.nodeType == 3) {
-			element.parentNode.removeChild(element);
-			return element;
-		}
-
-		if (element.childNodes.length == 0) {
-			console.debug("remove empty", element);
-			element.parentNode.removeChild(element);
-			return null;
-		}
-
-		return this.getFirstTextNode(element.childNodes[0]);
-	}
-
-	this.insertSeparately = function (element, node, child) {
-		if (!node.data || node.data.length == 0) {
-			return;
-		}
-
-		if (node.data.length == 1) {
-			this.insert(element, node, child);
-			return;
-		}
-
-		for (var i = 0; i < node.data.length; i++) {
-			this.insertSeparately(element, document.createTextNode(node.data.substr(i, 1)), child);
-		}
-	}
-
-	this.insert = function (element, node, child) {
-		if (this.cursorPosition > this.charCount) {
-			element.insertBefore(node, child);
-		} else {
-			if (child.nextSibling) {
-				element.insertBefore(node, child.nextSibling);
-			} else {
-				element.appendChild(node);
-			}
-		}
-		this.charCount ++;
-	}
+		return value;
+	};
 
 	this.getText = function() {
-			return this.area.val();
-	}
+		return this.area.val();
+	};
 
 	this.getObject = function() {
-			return this.area;
-	}
-
-	this.getCursorPosition = function() {
-		var cursorPos = null;
-		if (window.getSelection) {
-			var selObj = window.getSelection();
-			var selRange = selObj.getRangeAt(0);
-			var list = selObj.anchorNode.parentNode.childNodes;
-			for (var i = 0; i < list.length; i++) {
-				if (list[i] == selObj.anchorNode) {
-					cursorPos = i;
-					break;
-				}
-			}
-			if (cursorPos == null) {
-				cursorPos = -1;
-			}
-			cursorPos += selObj.anchorOffset;
-		} else if (document.selection) {
-			var range = document.selection.createRange();
-			var bookmark = range.getBookmark();
-			cursorPos = bookmark.charCodeAt(2) - 11; /* Undocumented function [3] */
-		}
-		return cursorPos;
-	}
-
+		return this.area;
+	};
+	
+	/**
+	 * Updates the infobox
+	 *
+	 * Sets button dis/enabled and update text
+	 */
 	this.updateInfobox = function() {
-		if (!this.response || this.response.error) {
-			this.infoBox.html('');
-			this.next.attr('disabled', 'disabled');
-			this.previous.attr('disabled', 'disabled');
-		} else if (this.response.matchings.length == 0) {
+
+		// if its no valid response or if no matchings
+		if (!this.response || this.response.error || this.response.matchings.length == 0) {
 			this.infoBox.html('no matches');
 			this.next.attr('disabled', 'disabled');
 			this.previous.attr('disabled', 'disabled');
+
+			// valid matchings
 		} else {
-			this.infoBox.html('match ' + (this.selectedMatch+1) + ' of ' + this.response.matchings.length);
+			this.infoBox.html('match ' + (this.selectedMatch + 1) + ' of '
+					+ this.response.matchings.length);
 
 			// disable/enable match buttons
 			if (this.selectedMatch == 0 || this.response.error) {
@@ -282,7 +196,8 @@ console.debug("cursor", this.cursorPosition);
 				this.previous.removeAttr('disabled');
 			}
 
-			if (this.selectedMatch >= this.response.matchings.length -1 || this.response.error) {
+			if (this.selectedMatch >= this.response.matchings.length - 1
+					|| this.response.error) {
 				this.next.attr('disabled', 'disabled');
 			} else {
 				this.next.removeAttr('disabled');
@@ -290,61 +205,96 @@ console.debug("cursor", this.cursorPosition);
 		}
 	};
 
-    this.getmoreInformation = function() {
-        this.updateMatches();
+	this.getmoreInformation = function() {
+		this.updateMatches();
 
-        if (this.error || this.matches.length == 0) {
-            return '';
-        }
+		if (this.error || this.matches.length == 0) {
+			return '';
+		}
 
-        var results = this.matches[this.currentMatching].results;
+		var results = this.matches[this.currentMatching].results;
 
-        if (results.length <= 0) {
-            return 'no subexpressions given.';
-        }
+		if (results.length <= 0) {
+			return 'no subexpressions given.';
+		}
 
-        var output = 'subexpressions <small>(expression parts in brackets)</small>:<ul>';
-        for (var i=0; i<results.length; i++) {
-            output += '<li>$'+i+' = ' + results[i] + '</li>';
-        }
-        output += '</ul>';
+		var output = 'subexpressions <small>(expression parts in brackets)</small>:<ul>';
+		for ( var i = 0; i < results.length; i++) {
+			output += '<li>$' + i + ' = ' + results[i] + '</li>';
+		}
+		output += '</ul>';
 
-        return output;
-    }
+		return output;
+	}
 
+	/**
+	 * Updates all information in and around the textbox
+	 * 
+	 * <ul>
+	 * <li>highlight the text</li>
+	 * <li>update the infobox</li>
+	 * </ul>
+	 */
 	this.update = function() {
 		this.highlight();
 		this.updateInfobox();
 	}
 
+	/**
+	 * Select next match if possible
+	 * 
+	 * @return this
+	 */
 	this.nextMatch = function() {
-		if (this.response && this.selectedMatch < this.response.matchings.length - 1) {
+		if (this.response
+				&& this.selectedMatch < this.response.matchings.length - 1) {
 			this.selectedMatch++;
 		}
 		this.update();
+
+		return this;
 	}
 
+	/**
+	 * Select previous match if possible
+	 * 
+	 * @return this
+	 */
 	this.previousMatch = function() {
 		if (this.selectedMatch > 0) {
 			this.selectedMatch--;
 		}
 		this.update();
+
+		return this;
+	}
+
+	/**
+	 * Open and close the information box
+	 * 
+	 * @param boolean
+	 *            open == true
+	 * @return this
+	 */
+	this.moreInformation = function(open) {
+		// TODO
+		return this;
 	}
 
 	// set button handler
-	this.next.click(function () {
+	this.next.click(function() {
 		$this.nextMatch()
 	});
-	this.previous.click(function () {
+	this.previous.click(function() {
 		$this.previousMatch();
 	});
 	/*
-	this.block.find('.match-more-info').find('a').click(function() {
-		RegHex.getMatchingBlock($(this).closest('.matchtext-block')).moreInfo();
-		return false;
-	});
-	this.block.find('.less-information').click(function() {
-		RegHex.getMatchingBlock($(this).closest('.matchtext-block')).lessInfo();
-		return false;
-	});*/
+	 * this.block.find('.match-more-info').find('a').click(function() {
+	 * RegHex.getMatchingBlock($(this).closest('.matchtext-block')).moreInfo();
+	 * return false; }); this.block.find('.less-information').click(function() {
+	 * RegHex.getMatchingBlock($(this).closest('.matchtext-block')).lessInfo();
+	 * return false; });
+	 */
+
+	this.update();
 }
