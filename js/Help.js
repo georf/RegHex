2de09efd24bf56ed2238cf66a7b7d5c82c5f5bca
georf <georf@georf.de>
2012-01-19 21:24:57 +0100

    First help prototype

---
+++ js/Help.js
@@ -0,0 +1,159 @@
+/**
+ * Generate a live help
+ *
+ * @author Georg Limbach <georf@dev.mgvmedia.com>
+ */
+
+function Help() {
+	var $this = this;
+	this.lastTimeout = null;
+	
+	
+	
+	this.close = function() {
+
+		$this.breakSteps();
+		
+		$('#help-content').hide();
+		$('#help-box').show();
+		
+	};
+	
+	
+	
+	
+	this.steps = new Array(
+		new Step(function() {
+			$('#help-start').hide();
+			
+			$('#help-content ol').addClass("deactive");
+		}, 100),
+		
+		new Step(function() {
+			
+			$('#help-step1').addClass("active");
+			
+			// let it blink 3 times
+			$('#parser-type').focus()
+			.fadeOut(500).fadeIn(500)
+			.fadeOut(500).fadeIn(500)
+			.fadeOut(500).fadeIn(500);	
+		}, 5000),
+		
+		new Step(function() {
+			$('#help-step1').removeClass("active");
+		}, 100),
+		
+		new Step(function() {
+			$('#help-step2').addClass("active");
+			
+			// add a match text
+			new TypeInto($('#matchtext').focus(), 'abb', 500);
+		}, 5000),
+		
+		new Step(function() {
+			$('#help-step2').removeClass("active");
+		}, 100),
+		
+		new Step(function() {
+			$('#help-step3').addClass("active");
+
+			// let plus blink 3 times
+			$('#add-matchtext').focus()
+			.fadeOut(500).fadeIn(500)
+			.fadeOut(500).fadeIn(500)
+			.fadeOut(500).fadeIn(500, function() {
+
+				$('#add-matchtext').click();
+			});
+			
+			
+			// add a match text
+			new TypeInto($('#matchtext').focus(), 'abbasdf', 500);
+		}, 5000),
+		
+		new Step(function() {
+			$('#help-step3').removeClass("active");
+		}, 100),
+		
+		new Step(function() {
+			
+			$('#help-step4').addClass("active");
+			
+			// add a regular expression
+			$('#regex').focus();
+			new TypeInto($('#regex'), 'a[a-z]+', 500);
+		}, 5000),
+		
+		new Step(function() {
+			$('#help-step4').removeClass("active");
+		}, 100),
+		
+		new Step(function() {
+			$('#help-content ol').removeClass("deactive");
+			$('#help-start').show();
+		}, 100)
+	);
+	
+	this.runSteps = function() {
+		var time = 0;
+		for ( var i = 0; i < $this.steps.length; i++) {
+			$this.steps[i].timeoutId = setTimeout($this.steps[i].run, time);
+			time += $this.steps[i].time;
+		}
+	};
+	
+	this.breakSteps = function() {
+		var time = 0;
+		for ( var i = 0; i < $this.steps.length; i++) {
+			if ($this.steps[i].timeoutId != null) {
+				clearTimeout($this.steps[i].timeoutId);
+				$this.steps[i].timeoutId = null;
+			}
+		}
+	};
+
+	// initiation
+	$('#help-box').hide();
+
+	$('#help-close').unbind().click($this.close);
+	$('#help-start').click($this.runSteps);
+	$('#help-start').show();
+	
+	$('#help-content').show();
+}
+
+function TypeInto(area, val, millisec) {
+	var $this = this;
+	this.area = area;
+	this.val = val;
+	this.millisec = millisec;
+	
+	this.step = 0;
+	
+	this.typeKey = function() {
+		$this.area.val($this.val.substring(0, $this.step));
+		$this.area.trigger('keyup');
+		
+		if ($this.step < $this.val.length) {
+			$this.step++;
+			setTimeout($this.typeKey, $this.millisec);
+		}
+	};
+	
+	this.typeKey();
+}
+
+function Step(callback, time) {
+	var $this = this;
+	
+	this.callback = callback;
+	this.time = time;
+	this.timeoutId = null;
+	
+	this.run = function() {
+		$this.callback();
+		
+		$this.timeoutId = null;
+	};
+}
